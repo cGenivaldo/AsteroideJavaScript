1e0c87a128d6f65d642ebead35b563e26e8814fc
Genivaldo <cgenivaldo46@gmail.com>
2022-02-22 19:29:56 -0300

    Finalizando controles

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ canvas.width = 1245
 canvas.height = 500
 
 const velocidade = 3
+const asceleracao = .1
+const desaceleracao = 30
 
 class GameObject {
-  constructor(x, y, radio, angle, color, giro){
+  constructor(x, y, radio, angle, color, spin, moving){
     this.x = x,
     this.y = y,
     this.radio = radio,
     this. angle = angle,
     this.color = color,
-    this.giro = giro
+    this.spin = spin,
+    this.moving = moving,
+    this.move = {
+      x:0,
+      y:0
+    }
   }
   draw() {
     context.fillStyle = this.color
-    context.lineWidth = 30 / 20
     context.beginPath()
     context.moveTo(
       this.x + this.radio * Math.cos(this.angle),
@@ -35,16 +41,19 @@ class GameObject {
  
 }
 
-const player = new GameObject(canvas.width / 2, canvas.height / 2, 10, 90 /180 * Math.PI, 'white',0)
+const player = new GameObject(canvas.width / 2, canvas.height / 2, 10, 90 /180 * Math.PI, 'white',0, false)
 const player2 = new GameObject(50,  50, 15, 90 /180 * Math.PI, 'black')
 
 document.addEventListener('keydown', ({keyCode}) => {
   switch(keyCode){
     case 65:
-      player.giro = velocidade / 180 * Math.PI 
+      player.spin = velocidade / 180 * Math.PI 
       break
+      case 87:
+        player.moving = true
+        break
     case 68:
-      player.giro = -velocidade / 180 * Math.PI 
+      player.spin = -velocidade / 180 * Math.PI 
       break
   }
 })
@@ -53,13 +62,32 @@ document.addEventListener('keydown', ({keyCode}) => {
 document.addEventListener('keyup', ({keyCode}) => {
   switch(keyCode){
     case 65:
-      player.giro = 0
+      player.spin = 0
+      break
+    case 87:
+      player.moving = false
       break
     case 68:
-      player.giro = 0
+      player.spin = 0
       break
   }
 })
+
+
+function movimentacaoPlayer() {
+  player.angle += player.spin
+
+  if(player.moving){
+    player.move.x += asceleracao * Math.cos(player.angle)
+    player.move.y -= asceleracao  * Math.sin(player.angle)
+  } else {
+    player.move.x -= player.move.x / desaceleracao
+    player.move.y -=  player.move.y / desaceleracao  
+  }
+
+  player.x += player.move.x
+  player.y += player.move.y
+}
 
 function gameSetUp() {
   requestAnimationFrame(gameSetUp)
@@ -67,8 +95,7 @@ function gameSetUp() {
   context.fillRect(0, 0, canvas.width, canvas.height)
   player2.draw()
   player.draw()
-
-  player.angle += player.giro
+  movimentacaoPlayer()
 }
 
 
